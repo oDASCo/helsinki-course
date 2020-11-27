72464dc70117a45db141306d15c6ff5175d8017f
oDASCo <dasc@ya.ru>
2020-11-27 18:14:46 +0300

    tasks till 1.14

--- src/App.js
+++ src/App.js
@@ -1,76 +1,41 @@
 import React, { useState } from 'react'
 
-const Button = ({handleClick, text}) => {
-    return (
-        <button onClick={handleClick}>{text}</button>
-    );
-};
-
-const Statistics = (props) => {
-    if (props.good || props.neutral || props.bad) {
-        return (
-            <div>
-                <table>
-                    <tbody>
-                    <Statistic text='Good' value={props.good}/>
-                    <Statistic text='Neutral' value={props.neutral}/>
-                    <Statistic text='Bad' value={props.bad}/>
-                    <Statistic text='All' value={props.stats.all}/>
-                    <Statistic text='Average' value={props.stats.average}/>
-                    <Statistic text='Positive' value={props.stats.positive}/>
-                    </tbody>
-                </table>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <p>No feedback given</p>
-            </div>
-        )
-    }
-};
-
-const Statistic = ({text, value}) => <tr><td>{text}:</td><td>{value}</td></tr>;
-
 const App = () => {
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
-    const stats = {
-        all: good + neutral + bad,
-        average: (good - bad) / (good + neutral + bad),
-        positive: (good * 100) / (good + neutral + bad)
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    ];
+    const [selected, setSelected] = useState(0);
+    const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0 });
+
+    const nextAnecdote = () => {
+        setSelected(Math.floor(Math.random() * (anecdotes.length)));
+    };
+    const vote = () => {
+        setVotes({
+            ...votes,
+            [selected]:  votes[selected] += 1
+        });
     };
 
-    const addGoodFB = () => {
-        setGood(good + 1);
-    };
-
-    const addNeutralFB = () => {
-        setNeutral(neutral + 1);
-    };
-
-    const addBadFB = () => {
-        setBad(bad + 1);
+    const getAnecdoteMaxVotes = () => {
+        return Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b);
     };
 
     return (
         <div>
-            <h2>Give feedback</h2>
-            <Button handleClick={addGoodFB} text='Good'/>
-            <Button handleClick={addNeutralFB} text='Neutral'/>
-            <Button handleClick={addBadFB} text='Bad'/>
-
-            <h3>Statistics</h3>
-
-            <Statistics stats={stats}
-                        good={good}
-                        neutral={neutral}
-                        bad={bad}/>
-
+            <h3>Anecdote of the day</h3>
+            <p>{anecdotes[selected]}</p>
+            <button onClick={nextAnecdote}>next anecdote</button>
+            <button onClick={vote}>vote</button>
+            <h3>Anecdote with  most votes</h3>
+            <p>{anecdotes[getAnecdoteMaxVotes()]}</p>
         </div>
     )
-}
+};
 
 export default App;
