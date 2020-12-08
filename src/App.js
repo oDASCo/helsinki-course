460b45c12f194780a48de299a307750f3e03592a
oDASCo <dasc@ya.ru>
2020-12-08 16:08:56 +0300

    2.12-2.14

--- src/App.js
+++ src/App.js
@@ -1,88 +1,88 @@
 import React, { useState,  useEffect } from 'react'
-import Filter from "./components/Filter";
-import AddForm from "./components/AddForm";
-import PhoneList from "./components/PhonesList";
 import axios from 'axios';
 
 const App = () => {
-    const [ listOfPersons, setListOfPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
-    const [ persons, setPersons ] = useState(listOfPersons);
-    const [ newName, setNewName ] = useState('');
-    const [ newNumber, setNewNumber ] = useState('');
+    const [ listOfCountries, setListOfCountries ] = useState([]);
+    const [ countries, setCountries ] = useState(listOfCountries);
     const [ newSearch, setSearch ] = useState('');
-
-    const handleNameChange = (event) => {
-        event.preventDefault();
-        setNewName(event.target.value);
-    };
-
-    const handleNumberChange = (event) => {
-        event.preventDefault();
-        setNewNumber(event.target.value);
-    };
+    //const [ showOne, setShowState ] = useState(false);
+    let country;
+    let content;
 
     useEffect(() => {
-        console.log('effect');
-
         const eventHandler = response => {
-            console.log('promise fulfilled')
-            setListOfPersons(response.data)
+            console.log('promise fulfilled');
+            setListOfCountries(response.data)
         };
 
-        const promise = axios.get('http://localhost:3001/persons');
+        const promise = axios.get('https://restcountries.eu/rest/v2/all');
         promise.then(eventHandler)
     }, []);
 
-    const addName = (event) => {
-        event.preventDefault();
-        if (persons.find(item => item.name === newName)) {
-           alert(`${newName} is already added to phonebook`);
-        } else {
-            setPersons(persons.concat({
-                name: newName,
-                number: newNumber
-            }));
-            setListOfPersons(listOfPersons.concat({
-                name: newName,
-                number: newNumber
-            }));
-            setNewName('');
-            setNewNumber('');
-        }
+    const showCountry = (countryName) => {
+        let country = countries.find(item => item.name === countryName);
+        console.log(country);
+        setCountries([country]);
     };
 
 
     const handleSearch = (event) => {
         if (event.target.value === '') {
             setSearch('');
-            setPersons(listOfPersons);
+            setCountries(listOfCountries);
         } else {
             setSearch(event.target.value);
-            let filteredPersons = listOfPersons.filter(person => person.name.toLowerCase().indexOf(newSearch.trim().toLowerCase()) !== -1);
-            setPersons(filteredPersons);
+            let filteredCountries = listOfCountries.filter(item => {
+                return item.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+            });
+            setCountries(filteredCountries);
         }
 
     };
-
+    if (countries.length > 10) {
+        content = <p>Too many matches</p>
+    } else if (countries.length === 1) {
+        country = countries[0];
+        if (country.name === 'Belarus') {
+            content = <div>
+                <h3>{country.name}</h3>
+                <p>capital: {country.capital}</p>
+                <p>population: {country.population}</p>
+                <p>Languages:</p>
+                <ul>
+                    {country.languages.map(item => <li>{item.name}</li>)}
+                </ul>
+                <img src={'https://upload.wikimedia.org/wikipedia/commons/5/50/Flag_of_Belarus_%281918%2C_1991%E2%80%931995%29.svg'}/>
+            </div>
+        } else {
+            content = <div>
+                <h3>{country.name}</h3>
+                <p>capital: {country.capital}</p>
+                <p>population: {country.population}</p>
+                <p>Languages:</p>
+                <ul>
+                    {country.languages.map(item => <li>{item.name}</li>)}
+                </ul>
+                <img src={country.flag}/>
+            </div>
+        }
+    } else {
+        content = <ul>
+            {countries.map((item) => {
+                    return (<li>{item.name} <button onClick={() => showCountry(item.name)}>show</button></li>)
+                }
+            )
+            }
+        </ul>;
+    }
     return (
         <div>
-            <h2>Phonebook</h2>
-            <Filter searchVal={newSearch} onSearchName={handleSearch}/>
-            <h3>Add new</h3>
-           <AddForm newName={newName}
-                    newNumber={newNumber}
-                    onNameChange={handleNameChange}
-                    onNumberChange={handleNumberChange}
-                    onSubmitAdd={addName}/>
-            <h2>Numbers</h2>
-            <PhoneList persons={persons}/>
+            <h2>Countries</h2>
+            <input onInput={handleSearch}/>
+            {content}
         </div>
     )
+
 };
 
 export default App;
